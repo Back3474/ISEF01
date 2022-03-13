9f47270239579c5ba9f238d4a657783077506a11
kk84iu <95606357+kk84iu@users.noreply.github.com>
2022-03-13 18:51:06 +0100

    Minimale Passwort Settings, 9 Stellen + nicht rein numerisch

--- ISEF01_QuizApp/settings.py
+++ ISEF01_QuizApp/settings.py
@@ -50,6 +50,18 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 9,
+        }
+    },
+	{
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
